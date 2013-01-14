200b0e9a91d03905005bda0803b9d83dad27eaa9
MunkiPhD <munkiphd@gmail.com>
2013-01-14 16:24:06 -0500

    mid adding starfield

---
+++ FiniteSpace/FiniteSpace/StarField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FiniteSpace {
+    class StarField {
+        private List<Sprite> _stars = new List<Sprite>();
+        private int _screenWidth = 800;
+        private int _screenHeight = 600;
+        private Random rand = new Random();
+        private Color[] colors = { Color.White, Color.Yellow, Color.Wheat, Color.WhiteSmoke, Color.SlateGray };
+
+        public StarField(int screenWidth, int screenHeight
+
+    }
+}
